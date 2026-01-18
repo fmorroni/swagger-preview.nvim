12f858c97950f2826bee2e45bb980a9889f304fb
Franco Morroni <fmorroni99@gmail.com>
2026-01-18 01:57:51 -0300

    feat: add server start/stop nvim user commands

--- lua/swagger-preview/init.lua
+++ lua/swagger-preview/init.lua
@@ -1,11 +1,19 @@
 local helpers = require("swagger-preview.helpers")
 
 local M = {}
-local job = nil
+---@type vim.SystemObj?
+local server_job = nil
+---@type vim.SystemObj?
+local browser_job = nil
 
 ---@class SwaggerPreview.SetupOptions
 ---@field port integer?
 ---@field app string[]?
+
+---@class SwaggerPreview.Message
+-----@field code integer
+---@field message string
+---@field level vim.log.levels
 
 ---@param opts SwaggerPreview.SetupOptions?
 M.setup = function(opts)
@@ -45,23 +53,29 @@ local start_handshake_socket = function(callback_on_port)
 end
 
 ---@param spec_path string
+---@return SwaggerPreview.Message?
 M.start_server = function(spec_path)
-	if job then
-		vim.notify("SwaggerPreview server is already running.", vim.log.levels.WARN)
-		return
+	if server_job then
+		---@type SwaggerPreview.Message
+		return { message = "SwaggerPreview server is already running.", level = vim.log.levels.WARN }
 	end
 
 	if not spec_path then
-		vim.notify("A path to the openapi specification file must be specified.", vim.log.levels.ERROR)
-		return
+		---@type SwaggerPreview.Message
+		return { message = "A path to the openapi specification file must be specified.", level = vim.log.levels.ERROR }
 	end
+
 	local socket_path = start_handshake_socket(function(port)
 		local cmd = { unpack(M.app) }
 		table.insert(cmd, "http://localhost:" .. port)
-		vim.system(cmd)
+		-- TODO: This is actually dumb. I don't need the port here, I can just open the browser
+		-- from deno along with the server.... Pass command through env var with `vim.json.encode(M.app)`.
+		browser_job = vim.system(cmd)
+		P({ "Browser start:", browser_job })
 	end)
+
 	M.spec_path = spec_path
-	job = vim.system({
+	server_job = vim.system({
 		"deno",
 		"run",
 		"--allow-env=OPENAPI_SPEC,PORT,HANDSHAKE_SOCKET",
@@ -86,16 +100,21 @@ M.start_server = function(spec_path)
 	end)
 end
 
+---@return SwaggerPreview.Message
 M.stop_server = function()
-	if not job then
-		vim.notify("SwaggerPreview server is not running.", vim.log.levels.WARN)
-		return
+	if not server_job then
+		return { message = "SwaggerPreview server is not running.", level = vim.log.levels.WARN }
 	end
 
-	job:kill("sigterm")
-	job = nil
+	server_job:kill("sigterm")
+	server_job = nil
 
-	vim.notify("SwaggerPreview server stopped.", vim.log.levels.INFO)
+	if browser_job then
+		browser_job:kill("sigkill")
+		browser_job = nil
+	end
+
+	return { message = "SwaggerPreview server stopped.", level = vim.log.levels.INFO }
 end
 
 return M
--- plugin/swagger_preview.lua
+++ plugin/swagger_preview.lua
@@ -1,1 +1,25 @@
--- Add things like commands here
+local sp = require("swagger-preview")
+
+vim.api.nvim_create_user_command("SwaggerPreviewStart", function()
+	local buf = vim.api.nvim_get_current_buf()
+	local spec_path = vim.api.nvim_buf_get_name(buf)
+
+	if spec_path == "" then
+		vim.notify("Current buffer has no file path.", vim.log.levels.ERROR)
+		return
+	end
+
+	local error = sp.start_server(spec_path)
+	if error then
+		vim.notify(error.message, error.level)
+	end
+end, {
+	desc = "Start Swagger Preview server for current buffer",
+})
+
+vim.api.nvim_create_user_command("SwaggerPreviewStop", function()
+	local msg = sp.stop_server()
+	vim.notify(msg.message, msg.level)
+end, {
+	desc = "Stop Swagger Preview server",
+})
